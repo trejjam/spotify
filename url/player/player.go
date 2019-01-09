492e4f9eb4b519b91f6e425b1bd583f26ec0d62a
Jan Trejbal <jan.trejbal@gmail.com>
2019-01-10 00:14:26 +0100

    Add player endpoints

---
+++ url/player/player.go
@@ -0,0 +1,17 @@
+package player
+
+const (
+	SkipUsersPlaybackToNextTrack              = "https://api.spotify.com/v1/me/player/next"
+	SeekToPositionInCurrentlyPlayingTrack     = "https://api.spotify.com/v1/me/player/seek"
+	GetUserAvailableDevices                   = "https://api.spotify.com/v1/me/player/devices"
+	ToggleShuffleForUserPlayback              = "https://api.spotify.com/v1/me/player/shuffle"
+	TransferUserPlayback                      = "https://api.spotify.com/v1/me/player"
+	GetCurrentUserRecentlyPlayedTracks        = "https://api.spotify.com/v1/me/player/recently-played"
+	StartUserPlayback                         = "https://api.spotify.com/v1/me/player/play"
+	SetRepeatModeOnUserPlayback               = "https://api.spotify.com/v1/me/player/repeat"
+	PauseUserPlayback                         = "https://api.spotify.com/v1/me/player/pause"
+	SkipUserPlaybackToPreviousTrack           = "https://api.spotify.com/v1/me/player/previous"
+	GetInformationAboutTheUserCurrentPlayback = "https://api.spotify.com/v1/me/player"
+	GetUserCurrentlyPlayingTrack              = "https://api.spotify.com/v1/me/player/currently-playing"
+	SetVolumeForUserPlayback                  = "https://api.spotify.com/v1/me/player/volume"
+)
