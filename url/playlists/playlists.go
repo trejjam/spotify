5e222911401453087f4cdc289eb1241b5adf9107
Jan Trejbal <jan.trejbal@gmail.com>
2019-01-10 00:01:09 +0100

    Add playlists endpoints

---
+++ url/playlists/playlists.go
@@ -0,0 +1,16 @@
+package playlists
+
+const (
+	AddTracks                      = "https://api.spotify.com/v1/playlists/%s/tracks"
+	GetPlaylist                    = "https://api.spotify.com/v1/playlists/%s"
+	RemoveTracks                   = "https://api.spotify.com/v1/playlists/%s/tracks"
+	GetPlaylistTracks              = "https://api.spotify.com/v1/playlists/%s/tracks"
+	GetPlaylistCoverImage          = "https://api.spotify.com/v1/playlists/%s/images"
+	UploadCustomPlaylistCoverImage = "https://api.spotify.com/v1/playlists/%s/images"
+	GetCurrentPlaylists            = "https://api.spotify.com/v1/me/playlists"
+	ChangePlaylist                 = "https://api.spotify.com/v1/playlists/%s"
+	GetListOfUsersPlaylists        = "https://api.spotify.com/v1/users/%s/playlists"
+	ReplacePlaylistsTracks         = "https://api.spotify.com/v1/playlists/%s/tracks"
+	CreateUserPlaylist             = "https://api.spotify.com/v1/users/%s/playlists"
+	ReorderPlaylistTracks          = "https://api.spotify.com/v1/playlists/%s/tracks"
+)
